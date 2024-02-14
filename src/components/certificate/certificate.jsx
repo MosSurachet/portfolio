a48d568c97540c863d874337bd26bc9883d20356
Surachet Intamongkol <diymos78@gmail.com>
2024-02-14 17:45:15 +0700

    Fixed bug

--- src/components/certificate/certificate.jsx
+++ src/components/certificate/certificate.jsx
@@ -10,24 +10,24 @@ function certificate() {
         <div className={styles.port_list}>
             <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                <a href="src\assets\cer_webapp_futureskill_page-0001.jpg" target='_blank'>
-                <img src="src\assets\cer_webapp_futureskill_page-0001.jpg" />
+                <a href="src\assets\cer-react.jpg" target='_blank'>
+                <img src="src\assets\cer-react.jpg" />
                 </a>
             </Tilt>
-            <p>สร้างเว็ปด้วย python</p>
-            </div>
-            <div className={styles.port_items}>
-            <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                <a href="src\assets\cer-react (1)_page-0001.jpg" target='_blank'>
-                <img src="src\assets\cer-react (1)_page-0001.jpg" alt="" />
-                </a>
-            </Tilt>            
             <p>ทำ Web portfolio ด้วย React</p>
             </div>
             <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
-                <a href="src\assets\Certificate-of-Participation-FWBRanger-9_page-0001.jpg" target='_blank'>
-                <img src="src\assets\Certificate-of-Participation-FWBRanger-9_page-0001.jpg" alt="" /></a>
+                <a href="src\assets\cer_webapp.jpg" target='_blank'>
+                <img src="src\assets\cer_webapp.jpg" alt="" />
+                </a>
+            </Tilt>            
+            <p>สร้างเว็ปด้วย python</p>
+            </div>
+            <div className={styles.port_items}>
+            <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
+                <a href="src/assets/CertificateFWB.jpg" target='_blank'>
+                <img src="src/assets/CertificateFWB.jpg" alt="" /></a>
             </Tilt>            
             <p>Financial Well-being HACKATHON 
                 <br />for Thais Powered by ttb</p>
