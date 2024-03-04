193a469d9c8dfc4e4ef8ee0243aacc9893632480
Surachet Intamongkol <diymos78@gmail.com>
2024-03-04 17:55:00 +0700

    Update all

--- src/components/certificate/certificate.jsx
+++ src/components/certificate/certificate.jsx
@@ -9,25 +9,25 @@ function certificate() {
         <h3 className={styles.port_title}>Certificate</h3>
         <div className={styles.port_list}>
             <div className={styles.port_items}>
-            <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
+            <Tilt scale={2} transitionSpeed={2500} tiltReverse={true}>
                 <a href="https://drive.google.com/file/d/12KW9cwI3uryVryo1feA1D9K8j5_GicU4/view?usp=sharing" target='_blank'>
-                <img src='/src/assets/cer-react.jpg' />
+                <img src='src/images/cer-react.jpg' alt='CerReact Image'/>
                 </a>
             </Tilt>
             <p>ทำ Web portfolio ด้วย React</p>
             </div>
             <div className={styles.port_items}>
-            <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
+            <Tilt scale={2} transitionSpeed={2500} tiltReverse={true}>
                 <a href="https://drive.google.com/file/d/15Qo9-OkaIqn-o5yMfzVRFWmOvgVOGD27/view?usp=sharing"target='_blank' >
-                <img src='/src/assets/cer_webapp.jpg'/>
+                <img src='src/images/cer_webapp.jpg' alt='CerWebapp Image'/>
                 </a>
             </Tilt>            
             <p>สร้างเว็ปด้วย python</p>
             </div>
             <div className={styles.port_items}>
-            <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
+            <Tilt scale={2} transitionSpeed={2500} tiltReverse={true}>
                 <a href="https://drive.google.com/file/d/1LllzNfNZlterOi-zeiWBtvAP8qGte8iX/view?usp=sharing" target='_blank'>
-                <img src= '/src/assets/CertificateFWB.jpg' /></a>
+                <img src= 'src/images/CertificateFWB.jpg' alt='CerFWB image'/></a>
             </Tilt>            
             <p>Financial Well-being HACKATHON 
                 <br />for Thais Powered by ttb</p>
